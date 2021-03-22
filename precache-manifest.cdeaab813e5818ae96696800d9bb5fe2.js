b76f1ffd32cb1c57f9cb04989ab36f47f73d78ab
Idil Shaie <elena.idil@gmail.com/>
2021-03-21 22:48:52 -0500

    Updates

---
+++ precache-manifest.cdeaab813e5818ae96696800d9bb5fe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c363d342788af067461bb11d6b71dc75",
+    "url": "/Employee-Directory-/index.html"
+  },
+  {
+    "revision": "f0502039a5cf0d2ec136",
+    "url": "/Employee-Directory-/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "41d131bcf898175d1ffe",
+    "url": "/Employee-Directory-/static/css/main.c445a03f.chunk.css"
+  },
+  {
+    "revision": "f0502039a5cf0d2ec136",
+    "url": "/Employee-Directory-/static/js/2.9ce86cb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory-/static/js/2.9ce86cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d131bcf898175d1ffe",
+    "url": "/Employee-Directory-/static/js/main.86e687c3.chunk.js"
+  },
+  {
+    "revision": "0e98d41602d6c4d6c2d5",
+    "url": "/Employee-Directory-/static/js/runtime-main.a7b66dbd.js"
+  }
+]);
